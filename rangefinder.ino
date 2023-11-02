b82dc831aef3dd5c0858463d120f9037b614846c
Nateno2149 <nathan.peck01@outlook.com>
2023-11-02 15:40:52 -0700

    updated with proper toggle switch code, but still not fully functional

--- rangefinder.ino
+++ rangefinder.ino
@@ -1,16 +1,17 @@
 #include "rgb_lcd.h"
 rgb_lcd lcd;
 uint8_t rangePin = 5;
-uint8_t togglePin = 4;
+uint8_t togglePin = 12;
 uint8_t calibratePin = 3;
-uint8_t prevtoggleState;
-uint8_t currtoggleState;
+uint8_t buttonHold = 0;
+uint8_t cmMode = 1;
+uint8_t inchMode = 0;
+
 
 void setup() {
   pinMode(togglePin, INPUT);
   Serial.begin(9600);
   lcd.begin(16, 1);
-  currtoggleState = digitalRead(togglePin);
 }
 
 float rangeFinder(uint8_t rangePin) {
@@ -36,24 +37,39 @@ float cmConversion() {
 }
 
 void loop() {
-  prevtoggleState = currtoggleState;
-  currtoggleState = digitalRead(togglePin);
-  if (prevtoggleState == LOW && currtoggleState == HIGH) {
+  if (digitalRead(togglePin) == HIGH && buttonHold == 0) {
+    buttonHold = 1;
+    if (cmMode == LOW) {
+      cmMode = HIGH;
+      inchMode = LOW;
+    }
+    if (inchMode == LOW) {
+      inchMode = HIGH;
+      cmMode = LOW;
+    }
+  }
+  if (digitalRead(togglePin) == LOW) {
+    buttonHold = 0;
+  }
+
+  if (cmMode == HIGH) {
     lcd.clear();
     lcd.print(cmConversion());
     lcd.setCursor(1, 1);
     delay(300);
-    Serial.println("centimeters");
-  } else {
+  }
+  if (inchMode == HIGH) {
     lcd.clear();
     lcd.print(inchConversion());
     lcd.setCursor(1, 1);
     delay(300);
-    Serial.println("inches");
   }
-  Serial.println(prevtoggleState);
-  Serial.println(currtoggleState);
+
 }
+
+
+
+
 
 
 //float calibrateMode() {}
